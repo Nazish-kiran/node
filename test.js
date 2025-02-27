8b3a95b1e7569331f8a94b5287510bf6e83bb70e
NazishKiran <nazishkirancosmos123@gmail.com>
2025-02-27 23:11:50 +0500

    form submittion

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ import {PORT2} from './env.js';
 import path from 'path';
 
 const app = express()
-const staticAbout = path.join(import.meta.url , 'public' , 'about.html')
-app.use(express.static('public'))
+const staticAbout = path.join(import.meta.dirname , 'public')
+app.use(express.static(staticAbout))
 
 app.get('/contact' , (req , res)=>{
-    res.send('<h1 style="font-family:cursive;">Contact page of express</h1>')
+    console.log(req.query);
  res.redirect('/')
 })
 app.listen(PORT2 , ()=>{
