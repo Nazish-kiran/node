9a1f23bdf78c597f9a3ad4a64c7232382b8a8583
NazishKiran <nazishkirancosmos123@gmail.com>
2025-02-27 23:18:32 +0500

    object as data

--- test.js
+++ test.js
@@ -5,11 +5,19 @@ import path from 'path';
 const app = express()
 const staticAbout = path.join(import.meta.dirname , 'public')
 app.use(express.static(staticAbout))
+app.use(express.urlencoded({extended:true}))
 
-app.get('/contact' , (req , res)=>{
-    console.log(req.query);
+// app.get('/contact' , (req , res)=>{
+//     console.log(req.query);
+//  res.redirect('/')
+// })
+
+app.post('/contact' , (req , res)=>{
+    console.log(req.body);
  res.redirect('/')
 })
+
+
 app.listen(PORT2 , ()=>{
     console.log(`server is running on port ${PORT2}`)
 })
