2a9a330394245400ba8d62151b7a6cf32d004a61
NazishKiran <nazishkirancosmos123@gmail.com>
2025-03-02 22:21:13 +0500

    ejs

--- express.js
+++ express.js
@@ -1,15 +1,12 @@
 import express from "express";
 import { PORT } from "./env.js";
-import path from "path";
-import { log } from "console";
+import router from "./src/routes/index.js";
 const app = express();
-
+app.set("view engine", "ejs");
+app.use(router);
 app.use(express.static("public"));
-
-
-
 
 // const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log("server is running on port 4000");
+  console.log(`server is running on port ${PORT}`);
 });
