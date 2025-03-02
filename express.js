08cb885d04222a90d8b451b6da73d0336210916b
NazishKiran <nazishkirancosmos123@gmail.com>
2025-03-02 14:52:26 +0500

    routes

--- express.js
+++ express.js
@@ -6,34 +6,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-const notFoundPagePath = path.join(import.meta.dirname, "public", "404.html");
 
-app.get("/", (req, res) => {
-  res.send("<h1 style='font-family:cursive;'>Home page of express </h1>");
-});
-app.get("/about/:username", (req, res) => {
-  console.log(req.params);
-  res.send(
-    `<h1 style='font-family:cursive;'>About page of express by ${req.params.username} </h1>`
-  );
-});
-app.get("/about/:username/article/:slug", (req, res) => {
-  console.log(req.params);
-  const formattedSlug = req.params.slug.replace(/-/g , " ")
-  res.send(
-    `<h1 style='font-family:cursive;'>About page of express by ${req.params.username} writting an article  on ${formattedSlug}</h1>`
-  );
-});
-
-app.get("/contact", (req, res) => {
-  res.send("<h1 style='font-family:cursive;'>Contact page of express </h1>");
-});
-app.get("/product", (req, res) => {
-  res.send(`<h1 style='font-family:cursive;'> user searched for ${req.query.search}</h1>`);
-});
-app.use((req , res)=>{
-  res.status(404).sendFile(notFoundPagePath)
-})
 
 
 // const PORT = process.env.PORT || 4000;
---
+++ src/routes/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import path from "path";
+
+const router = express.Router();
+const notFoundPagePath = path.join(import.meta.dirname, "public", "404.html");
+
+router.get("/", (req, res) => {
+  res.send("<h1 style='font-family:cursive;'>Home page of express </h1>");
+});
+router.get("/about/:username", (req, res) => {
+  console.log(req.params);
+  res.send(
+    `<h1 style='font-family:cursive;'>About page of express by ${req.params.username} </h1>`
+  );
+});
+router.get("/about/:username/article/:slug", (req, res) => {
+  console.log(req.params);
+  const formattedSlug = req.params.slug.replace(/-/g, " ");
+  res.send(
+    `<h1 style='font-family:cursive;'>About page of express by ${req.params.username} writting an article  on ${formattedSlug}</h1>`
+  );
+});
+
+router.get("/contact", (req, res) => {
+  res.send("<h1 style='font-family:cursive;'>Contact page of express </h1>");
+});
+router.get("/product", (req, res) => {
+  res.send(
+    `<h1 style='font-family:cursive;'> user searched for ${
+      req.query.search || "nothing"
+    }</h1>`
+  );
+});
+router.use((req, res) => {
+  res.status(404).sendFile(notFoundPagePath);
+});
+
+export default router;
